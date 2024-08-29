be4821ed65e966e993d3a7d50272f4befe0f9cc2
blockplacer4 <blockplacer4@gmail.com>
2024-08-29 19:28:38 +0200

    0.0.2
    Github Workflow added
    fixed issues

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,18 @@ FROM debian:buster-slim
 
 LABEL author="Janosch | Solus Labs" maintainer="info@janosch-bl.de"
 
-RUN apt update && apt upgrade -y \
-    apt install git wget tar zip unzip net-tools netcat telnet python3 python3-pip \
-    update-locale lang=de_DE.UTF-8 \
-    dpkg-reconfigure --frontend noninteractive locales \
+RUN apt update && apt upgrade -y && \
+    apt install -y git curl wget tar zip unzip net-tools netcat telnet python3 python3-pip locales && \
+    update-locale lang=de_DE.UTF-8 && \
+    dpkg-reconfigure --frontend noninteractive locales && \
     useradd -m -d /home/container -s /bin/bash container
 
 USER container
 
 ENV USER=container HOME=/home/container
-ENV DEBIAN_FRONTEND noninteractive
+ENV DEBIAN_FRONTEND=noninteractive
 
 WORKDIR /home/container
 
 COPY ./entrypoint.sh /entrypoint.sh
-CMD [ "bin/bash", "/entrypoint.sh"]
+CMD ["/bin/bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Farben und Textstile
-NC='\033[0m'
-PURPLE='\033[0;35m'
-LPURPLE='\033[1;35m'
-DGRAY='\033[1;30m'
-YELLOW='\033[1;33m'
+NC='\033[0m'            # No Color (Reset)
+BLUE='\033[0;34m'        # Blau
+CYAN='\033[0;36m'        # Heller Cyan
+LIGHT_BLUE='\033[1;34m'  # Hellblau
+DGRAY='\033[1;30m'       # Dunkelgrau
+YELLOW='\033[1;33m'      # Gelb
+WHITE='\033[1;37m'       # Weiß
+
+
 
 # Funktionen
 header() {
@@ -26,7 +30,7 @@ header() {
 
 load_java() {
     if [[ ! -d "/home/container/.jabba" ]]; then
-        echo -e "${PURPLE}Jabba not found! Installing Jabba..."
+        echo -e "${CYAN}Jabba not found! Installing Jabba..."
         curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc | awk -W interactive -v c="$DGRAY" '{ print c $0 }'
         . /home/container/.jabba/jabba.sh
     fi
@@ -42,7 +46,7 @@ load_java() {
         *)  echo -e "${YELLOW}Invalid Java version!"; exit 1 ;;
     esac
 
-    echo -e "${PURPLE}Installing Java ${LPURPLE}${JAVA_VERSION}${PURPLE}..."
+    echo -e "${CYAN}Installing Java ${LIGHT_BLUE}${JAVA_VERSION}${CYAN}..."
     jabba install "$JAVA_VERSION" > /dev/null 2>&1
     jabba use "$JAVA_VERSION"
 }
@@ -72,32 +76,32 @@ compile_spigot() {
 install_spigot() {
     load_java "$1"
     local SPIGOT_VERSION=$(curl -s https://hub.spigotmc.org/versions/ | grep -oP '"\K[^"]+')
-    echo -e "${PURPLE}Installing Spigot ${LPURPLE}${SPIGOT_VERSION}"
+    echo -e "${CYAN}Installing Spigot ${LIGHT_BLUE}${SPIGOT_VERSION}"
     compile_spigot "$SPIGOT_VERSION"
 }
 
 install_purpur() {
     local PURPUR_VERSION=$(curl -s https://api.purpurmc.org/v2/purpur | jq -r '.versions[-1]')
-    echo -e "${PURPLE}Installing Purpur ${LPURPLE}${PURPUR_VERSION}"
+    echo -e "${CYAN}Installing Purpur ${LIGHT_BLUE}${PURPUR_VERSION}"
     download_file "https://api.purpurmc.org/v2/purpur/${PURPUR_VERSION}/latest/download" "server.jar"
 }
 
 install_paper() {
     local PAPER_VERSION=$(curl -s https://papermc.io/api/v2/projects/paper | jq -r '.versions[-1]')
     local LATEST_BUILD=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${PAPER_VERSION}" | jq -r '.builds[-1]')
-    echo -e "${PURPLE}Installing PaperMC ${LPURPLE}${PAPER_VERSION}-${LATEST_BUILD}"
+    echo -e "${CYAN}Installing PaperMC ${LIGHT_BLUE}${PAPER_VERSION}-${LATEST_BUILD}"
     download_file "https://papermc.io/api/v2/projects/paper/versions/${PAPER_VERSION}/builds/${LATEST_BUILD}/downloads/paper-${PAPER_VERSION}-${LATEST_BUILD}.jar" "server.jar"
 }
 
 install_vanilla() {
     local VERSION=$(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.latest.release')
-    echo -e "${PURPLE}Installing Vanilla Minecraft ${LPURPLE}${VERSION}"
+    echo -e "${CYAN}Installing Vanilla Minecraft ${LIGHT_BLUE}${VERSION}"
     download_file "https://s3.amazonaws.com/Minecraft.Download/versions/${VERSION}/minecraft_server.${VERSION}.jar" "server.jar"
 }
 
 install_forge() {
     local VERSION=$(curl -s https://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions_slim.json | jq -r '.promos["1.16.5-latest"]')
-    echo -e "${PURPLE}Installing Forge ${LPURPLE}${VERSION}"
+    echo -e "${CYAN}Installing Forge ${LIGHT_BLUE}${VERSION}"
     download_file "https://files.minecraftforge.net/maven/net/minecraftforge/forge/${VERSION}/forge-${VERSION}-installer.jar" "forge-installer.jar"
     java -jar forge-installer.jar --installServer
     mv forge-*.jar server.jar
@@ -105,63 +109,63 @@ install_forge() {
 
 install_fabric() {
     local FABRIC_VERSION=$(curl -s https://meta.fabricmc.net/v2/versions/loader | jq -r '.[0].version')
-    echo -e "${PURPLE}Installing Fabric ${LPURPLE}${FABRIC_VERSION}"
+    echo -e "${CYAN}Installing Fabric ${LIGHT_BLUE}${FABRIC_VERSION}"
     download_file "https://meta.fabricmc.net/v2/versions/loader/${FABRIC_VERSION}/profile/json" "fabric-installer.json"
     java -jar fabric-installer.json --installServer
 }
 
 install_velocity() {
     local VELOCITY_VERSION=$(curl -s https://api.papermc.io/v2/projects/velocity | jq -r '.versions[-1]')
-    echo -e "${PURPLE}Installing Velocity ${LPURPLE}${VELOCITY_VERSION}"
+    echo -e "${CYAN}Installing Velocity ${LIGHT_BLUE}${VELOCITY_VERSION}"
     download_file "https://api.papermc.io/v2/projects/velocity/versions/${VELOCITY_VERSION}/latest/download" "server.jar"
 }
 
 install_waterfall() {
     local WATERFALL_VERSION=$(curl -s https://papermc.io/api/v2/projects/waterfall | jq -r '.versions[-1]')
-    echo -e "${PURPLE}Installing Waterfall ${LPURPLE}${WATERFALL_VERSION}"
+    echo -e "${CYAN}Installing Waterfall ${LIGHT_BLUE}${WATERFALL_VERSION}"
     download_file "https://papermc.io/api/v2/projects/waterfall/versions/${WATERFALL_VERSION}/builds/latest/download" "server.jar"
 }
 
 install_bungeecord() {
     local BUNGEE_VERSION=$(curl -s https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target | grep -oP 'BungeeCord-\d+\.jar')
-    echo -e "${PURPLE}Installing Bungeecord ${LPURPLE}${BUNGEE_VERSION}"
+    echo -e "${CYAN}Installing Bungeecord ${LIGHT_BLUE}${BUNGEE_VERSION}"
     download_file "https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/${BUNGEE_VERSION}" "server.jar"
 }
 
 install_pycord() {
-    echo -e "${PURPLE}Installing Pycord..."
+    echo -e "${CYAN}Installing Pycord..."
     pip install py-cord
 }
 
 install_discord_py() {
-    echo -e "${PURPLE}Installing discord.py..."
+    echo -e "${CYAN}Installing discord.py..."
     pip install discord.py
 }
 
 install_flask() {
-    echo -e "${PURPLE}Installing Flask..."
+    echo -e "${CYAN}Installing Flask..."
     pip install Flask
 }
 
 install_fastapi() {
-    echo -e "${PURPLE}Installing FastAPI..."
+    echo -e "${CYAN}Installing FastAPI..."
     pip install fastapi
 }
 
 start_server() {
-    echo -e "${PURPLE}Starting Minecraft server..."
+    echo -e "${CYAN}Starting Minecraft server..."
     MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
     ${MODIFIED_STARTUP}
 }
 
 menu_minecraft_proxy() {
     header
-    echo -e "${YELLOW}1${LPURPLE}) ${PURPLE}Install Velocity"
-    echo -e "${YELLOW}2${LPURPLE}) ${PURPLE}Install Waterfall"
-    echo -e "${YELLOW}3${LPURPLE}) ${PURPLE}Install Bungeecord"
-    echo -e "${YELLOW}0${LPURPLE}) ${PURPLE}Back"
+    echo -e "${YELLOW}1${LIGHT_BLUE}) ${CYAN}Install Velocity"
+    echo -e "${YELLOW}2${LIGHT_BLUE}) ${CYAN}Install Waterfall"
+    echo -e "${YELLOW}3${LIGHT_BLUE}) ${CYAN}Install Bungeecord"
+    echo -e "${YELLOW}0${LIGHT_BLUE}) ${CYAN}Back"
     
-    read -r -p "$(echo -e "${PURPLE}Select an option: ${LPURPLE}")" option
+    read -r -p "$(echo -e "${CYAN}Select an option: ${LIGHT_BLUE}")" option
 
     case $option in
         1) install_velocity ;;
@@ -174,15 +178,15 @@ menu_minecraft_proxy() {
 
 menu_minecraft_java() {
     header
-    echo -e "${YELLOW}1${LPURPLE}) ${PURPLE}Install PaperMC"
-    echo -e "${YELLOW}2${LPURPLE}) ${PURPLE}Install Purpur"
-    echo -e "${YELLOW}3${LPURPLE}) ${PURPLE}Install Spigot"
-    echo -e "${YELLOW}4${LPURPLE}) ${PURPLE}Install Vanilla"
-    echo -e "${YELLOW}5${LPURPLE}) ${PURPLE}Install Forge"
-    echo -e "${YELLOW}6${LPURPLE}) ${PURPLE}Install Fabric"
-    echo -e "${YELLOW}0${LPURPLE}) ${PURPLE}Back"
+    echo -e "${YELLOW}1${LIGHT_BLUE}) ${CYAN}Install PaperMC"
+    echo -e "${YELLOW}2${LIGHT_BLUE}) ${CYAN}Install Purpur"
+    echo -e "${YELLOW}3${LIGHT_BLUE}) ${CYAN}Install Spigot"
+    echo -e "${YELLOW}4${LIGHT_BLUE}) ${CYAN}Install Vanilla"
+    echo -e "${YELLOW}5${LIGHT_BLUE}) ${CYAN}Install Forge"
+    echo -e "${YELLOW}6${LIGHT_BLUE}) ${CYAN}Install Fabric"
+    echo -e "${YELLOW}0${LIGHT_BLUE}) ${CYAN}Back"
     
-    read -r -p "$(echo -e "${PURPLE}Select an option: ${LPURPLE}")" option
+    read -r -p "$(echo -e "${CYAN}Select an option: ${LIGHT_BLUE}")" option
 
     case $option in
         1) install_paper ;;
@@ -198,13 +202,13 @@ menu_minecraft_java() {
 
 menu_python() {
     header
-    echo -e "${YELLOW}1${LPURPLE}) ${PURPLE}Install Pycord"
-    echo -e "${YELLOW}2${LPURPLE}) ${PURPLE}Install discord.py"
-    echo -e "${YELLOW}3${LPURPLE}) ${PURPLE}Install Flask"
-    echo -e "${YELLOW}4${LPURPLE}) ${PURPLE}Install FastAPI"
-    echo -e "${YELLOW}0${LPURPLE}) ${PURPLE}Back"
+    echo -e "${YELLOW}1${LIGHT_BLUE}) ${CYAN}Install Pycord"
+    echo -e "${YELLOW}2${LIGHT_BLUE}) ${CYAN}Install discord.py"
+    echo -e "${YELLOW}3${LIGHT_BLUE}) ${CYAN}Install Flask"
+    echo -e "${YELLOW}4${LIGHT_BLUE}) ${CYAN}Install FastAPI"
+    echo -e "${YELLOW}0${LIGHT_BLUE}) ${CYAN}Back"
     
-    read -r -p "$(echo -e "${PURPLE}Select an option: ${LPURPLE}")" option
+    read -r -p "$(echo -e "${CYAN}Select an option: ${LIGHT_BLUE}")" option
 
     case $option in
         1) install_pycord ;;
@@ -218,18 +222,18 @@ menu_python() {
 
 main_menu() {
     header
-    echo -e "${YELLOW}1${LPURPLE}) ${PURPLE}Minecraft Proxy"
-    echo -e "${YELLOW}2${LPURPLE}) ${PURPLE}Minecraft Java Edition Server"
-    echo -e "${YELLOW}3${LPURPLE}) ${PURPLE}Python"
-    echo -e "${YELLOW}0${LPURPLE}) ${PURPLE}Exit"
+    echo -e "${YELLOW}1${LIGHT_BLUE}) ${CYAN}Minecraft Proxy"
+    echo -e "${YELLOW}2${LIGHT_BLUE}) ${CYAN}Minecraft Java Edition Server"
+    echo -e "${YELLOW}3${LIGHT_BLUE}) ${CYAN}Python"
+    echo -e "${YELLOW}0${LIGHT_BLUE}) ${CYAN}Exit"
 
-    read -r -p "$(echo -e "${PURPLE}Select an option: ${LPURPLE}")" option
+    read -r -p "$(echo -e "${CYAN}Select an option: ${LIGHT_BLUE}")" option
 
     case $option in
         1) menu_minecraft_proxy ;;
         2) menu_minecraft_java ;;
         3) menu_python ;;
-        0) echo -e "${PURPLE}Exiting..."; exit 0 ;;
+        0) echo -e "${CYAN}Exiting..."; exit 0 ;;
         *) echo -e "${DGRAY}Invalid option!"; main_menu ;;
     esac
 }
